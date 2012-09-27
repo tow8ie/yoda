f91bef003d41898e11ca8c21db70136e39225602
rubiii <me@rubiii.com>
2012-09-27 17:03:12 +0200

    use mainFilePath to require main.js

--- js/compiler.js
+++ js/compiler.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 
-var YUI = require('yui').YUI,
-    path = require('path'),
-    fs = require('fs'),
-    application = require('./main.js');
-
-var argv = require('optimist')
+var path = require('path'),
+    fs   = require('fs'),
+    argv = require('optimist')
            .usage('Usage: $0 --base-dir js/ --module moduleName --output-file path/to/file.js')
            .demand(['base-dir', 'module', 'output-file'])
            .alias('b', 'base-dir')
@@ -17,15 +14,20 @@ var argv = require('optimist')
            .describe('output-file', 'Path to compiled JS file')
            .argv;
 
-var baseDirPath = path.resolve(path.join('.', argv['base-dir']));
-var moduleToLoad = argv.module;
+
+var baseDirPath    = path.resolve(path.join('.', argv['base-dir']));
+var mainFilePath   = path.join(baseDirPath, 'main.js');
+var moduleToLoad   = argv.module;
 var outputFilePath = path.resolve(path.join('.', argv['output-file']));
+
+var YUI  = require('yui').YUI,
+    main = require(mainFilePath);
 
 console.log('Base Dir path:', baseDirPath);
 console.log('Module to load:', moduleToLoad);
 console.log('Output file path:', outputFilePath);
 
-var config = YUI().merge(application.config, {
+var config = YUI().merge(main.config, {
   base: './js/'
 });
 
@@ -71,9 +73,7 @@ YUI(config).use(moduleToLoad, function (Y) {
     str.push(fs.readFileSync(file, 'utf8'));
   });
     
-  var mainFilePath = path.join(baseDirPath, 'main.js');
   str.push(fs.readFileSync(mainFilePath, 'utf8'));
-
   fs.writeFileSync(outputFilePath, str.join('\n'), 'utf8');
 
 });
