605a5103518206b147c88ccaace544019e31b7b0
rubiii <me@rubiii.com>
2012-09-27 17:19:21 +0200

    added optional compression via UglifyJS

--- js/compiler.js
+++ js/compiler.js
@@ -3,22 +3,25 @@
 var path = require('path'),
     fs   = require('fs'),
     argv = require('optimist')
-           .usage('Usage: $0 --base-dir js/ --module moduleName --output-file path/to/file.js')
+           .usage('Usage: $0 --base-dir js/ --module moduleName --output-file path/to/file.js [options]')
            .demand(['base-dir', 'module', 'output-file'])
            .alias('b', 'base-dir')
            .alias('m', 'module')
            .alias('o', 'output-file')
+           .alias('c', 'compress')
            .describe('base-dir', 'Path to directory where the JS files can be found on disc. ' +
                                  'This directory must also include the main.js file.')
-           .describe('module', 'YUI module name (not path) of the first module to load')
-           .describe('output-file', 'Path to compiled JS file')
+           .describe('module', 'YUI module name (not path) of the first module to load.')
+           .describe('output-file', 'Path to compiled JS file.')
+           .describe('compress', 'Whether to compress the JavaScript code with UglifyJS.')
            .argv;
 
 
-var baseDirPath    = path.resolve(path.join('.', argv['base-dir']));
-var mainFilePath   = path.join(baseDirPath, 'main.js');
-var moduleToLoad   = argv.module;
+var baseDirPath = path.resolve(path.join('.', argv['base-dir']));
+var mainFilePath = path.join(baseDirPath, 'main.js');
+var moduleToLoad = argv.module;
 var outputFilePath = path.resolve(path.join('.', argv['output-file']));
+var compressionRequested = !!argv.compress;
 
 var YUI  = require('yui').YUI,
     main = require(mainFilePath);
@@ -26,6 +29,7 @@ var YUI  = require('yui').YUI,
 console.log('Base Dir path:', baseDirPath);
 console.log('Module to load:', moduleToLoad);
 console.log('Output file path:', outputFilePath);
+console.log('Compression:', compressionRequested);
 
 var config = YUI().merge(main.config, {
   base: './js/'
@@ -59,6 +63,16 @@ var resolveModules = function (modules) {
   return loader.resolve(true);
 }
 
+var compress = function (javascript) {
+  var parser = require("uglify-js").parser;
+  var uglify = require("uglify-js").uglify;
+
+  var ast = parser.parse(javascript);
+  ast = uglify.ast_mangle(ast);
+  ast = uglify.ast_squeeze(ast);
+  return uglify.gen_code(ast);
+}
+
 YUI(config).use(moduleToLoad, function (Y) {
 
   modules.push(moduleToLoad);
@@ -72,9 +86,16 @@ YUI(config).use(moduleToLoad, function (Y) {
   resolvedModules.forEach(function(file) {
     str.push(fs.readFileSync(file, 'utf8'));
   });
-    
+
+
   str.push(fs.readFileSync(mainFilePath, 'utf8'));
-  fs.writeFileSync(outputFilePath, str.join('\n'), 'utf8');
+  var output = str.join('\n');
+
+  if (compressionRequested) {
+    output = compress(output);
+  }
+
+  fs.writeFileSync(outputFilePath, output, 'utf8');
 
 });
 
